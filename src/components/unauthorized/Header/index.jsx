fb05b12993dd7ca7edb68d9d97a64488b61d0cb8
Lilianada <lilibest30.lb@gmail.com>
2023-05-23 23:31:05 +0100

    m

--- src/components/unauthorized/Header/index.jsx
+++ src/components/unauthorized/Header/index.jsx
@@ -1,18 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import logo from "../../../assets/imgs/logo.png";
+import { GrClose } from "react-icons/gr";
+import { FiMenu } from "react-icons/fi";
 import "./style.scss";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
-    const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-    const isActive = () => {
-      setShowMenu(!showMenu);
-    };
-  
-    const closeMenu = () => {
-      setShowMenu(false);
-    };
-    
+  const isActive = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
     <header className="unauthorized__header">
       <div className="desktop__header">
@@ -23,33 +26,106 @@ export default function Header() {
         <nav className="nav__bar">
           <ul className="nav__list">
             <li className="nav__item">
-              <a href="/" className="nav__link">
+              <NavLink
+                to="/resources"
+                className={({ isActive }) =>
+                  isActive ? "active__link" : "nav__link"
+                }
+              >
                 Resources
-              </a>
+              </NavLink>
             </li>
             <li className="nav__item">
-              <a href="/" className="nav__link">
+              <NavLink
+                to="/start-writing"
+                className={({ isActive }) =>
+                  isActive ? "active__link" : "nav__link"
+                }
+              >
                 Start Writing
-              </a>
+              </NavLink>
             </li>
             <li className="nav__item">
-              <a href="/" className="nav__link">
+              <NavLink
+                to="/signin"
+                className={({ isActive }) =>
+                  isActive ? "active__link" : "nav__link"
+                }
+              >
                 Sign In
-              </a>
+              </NavLink>
             </li>
             <li className="nav__item">
-              <a href="/" className="nav__button">
+              <NavLink to="/signup" className="nav__button">
                 Get Started
-              </a>
+              </NavLink>
             </li>
           </ul>
         </nav>
       </div>
+
+    {/* Mobile Header */}
       <div className="mobile__header">
         <div className="header__logo">
           <img src={logo} className="logo" alt="Chatter logo" />
           <h2 className="logo__name">Chatter</h2>
         </div>
+        <button type="button" className="menu__button">
+          <FiMenu size={32} stroke="white" fill="white" onClick={isActive} />
+        </button>
+
+        <nav className={`nav__bar ${showMenu ? "show__navbar" : "nav__bar"}`}>
+          <button type="button" className="close__menu" onClick={closeMenu}>
+            <GrClose />
+          </button>
+          <ul className="nav__list">
+            <li className="nav__item">
+              <h6 className="nav__title">Menu</h6>
+            </li>
+          </ul>
+
+          {/* Navigation List */}
+          <ul className="nav__list">
+            <li className="nav__item">
+            <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active__link" : "nav__link"
+                }
+                to="/about"
+              >
+                Resources
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active__link" : "nav__link"
+                }
+                to="/about"
+              >
+                Start Writing
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active__link" : "nav__link"
+                }
+                to="/about"
+              >
+                Sign In
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink to="/" className="nav__button">
+                Get Started
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.scss';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  // {
+  //   path: "/about",
+  //   element: <AboutUs />,
+  // },
+  // {
+  //   path: "/contact",
+  //   element: <Contact />,
+  // },
+  // {
+  //   path: "/partner-with-us",
+  //   element: <Partner/>, 
+  // },
+  // {
+  //   path: "/registration-form",
+  //   element: <Registration/>
+  // },
+  // {
+  //   path: "/terms-and-conditions",
+  //   element: <Terms/>
+  // },
+  // {
+  //   path: "/privacy-policies",
+  //   element: <Policies/>
+  // },
+  // {
+  //   path: "*",
+  //   element: <Error />,
+  // },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  // <React.StrictMode>
+  // </React.StrictMode>
 );
