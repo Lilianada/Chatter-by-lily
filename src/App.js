aec27912bbe56b03a39d26a2a905df05e531d1a2
Lilianada <lilibest30.lb@gmail.com>
2023-05-23 23:34:06 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/unauthorized/Header';
+import Hero from './components/unauthorized/Hero';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Hero/>
     </div>
   );
 }
