0ce9f420ce7134580456c3432227bdc36c5bc7a3
Lilianada <lilibest30.lb@gmail.com>
2023-05-24 23:32:51 +0100

    Featured posts

---
+++ src/components/unauthorized/FeaturePosts/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function FeaturedPosts() {
+  return (
+    <div className='featured__component'>
+      <div className="featured__content">
+        <div className="popular__post">
+            
+        </div>
+        <div className="featured__posts">
+            {
+
+            }
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/unauthorized/Hero/index.jsx
+++ src/components/unauthorized/Hero/index.jsx
@@ -7,18 +7,19 @@ export default function Hero() {
       <div className="hero__content">
         <div className="hero__text">
           <h1 className="hero__title">
-            Today 
+            Today
             <span className="hero__title_colored"> a writer</span>
             <br />
             Tomorrow
             <span className="hero__title_colored"> a leader</span>
           </h1>
           <h6 className="hero__description">
-          Everyone has an opinion that needs to be heard, we provide you the platform that helps you get heard. 
-          Enjoy beautiful stories written by talented writers.
+            Everyone has an opinion that needs to be heard, we provide you the
+            platform that helps you get heard. Enjoy beautiful stories written
+            by talented writers.
           </h6>
         </div>
-          <button className="hero__button">Get started now</button>
+        <button className="hero__button">Get started now</button>
         <div className="hero__image"></div>
       </div>
     </div>
