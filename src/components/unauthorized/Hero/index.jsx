21a04dd513b424af7b6edf13be119bdfb729cc7a
Lilianada <lilibest30.lb@gmail.com>
2023-05-24 23:03:59 +0100

    Hero texts and image

--- src/components/unauthorized/Hero/index.jsx
+++ src/components/unauthorized/Hero/index.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./style.scss";
 
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="hero__component">
+      <div className="hero__content">
+        <div className="hero__text">
+          <h1 className="hero__title">
+            Today 
+            <span className="hero__title_colored"> a writer</span>
+            <br />
+            Tomorrow
+            <span className="hero__title_colored"> a leader</span>
+          </h1>
+          <h6 className="hero__description">
+          Everyone has an opinion that needs to be heard, we provide you the platform that helps you get heard. 
+          Enjoy beautiful stories written by talented writers.
+          </h6>
+        </div>
+          <button className="hero__button">Get started now</button>
+        <div className="hero__image"></div>
+      </div>
+    </div>
+  );
 }
