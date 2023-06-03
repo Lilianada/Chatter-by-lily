9a877e1587fe99590980596c5e21df1a25e03935
Lilianada <lilibest30.lb@gmail.com>
2023-06-03 22:33:19 +0100

    Firebase installed

---
+++ src/services/Firebase/Firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGMce2c5yjQ4lbRHcco8pJGa3UZaFFUWE",
+  authDomain: "chatter-app-f65ae.firebaseapp.com",
+  projectId: "chatter-app-f65ae",
+  storageBucket: "chatter-app-f65ae.appspot.com",
+  messagingSenderId: "198866169869",
+  appId: "1:198866169869:web:95ac9df0638a05a6492544",
+  measurementId: "G-ERHX8V7RM4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
