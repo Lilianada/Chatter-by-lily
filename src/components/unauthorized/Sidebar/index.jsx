2595657cb42c53a7e4e1e93ca79b9895ce7dc5b4
Lilianada <lilibest30.lb@gmail.com>
2023-07-12 06:50:19 +0100

    chnages

--- src/components/unauthorized/Sidebar/index.jsx
+++ src/components/unauthorized/Sidebar/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import "./style.scss";
 import { Categories, Posts } from "../../../mockData";
 import { BsDot } from "react-icons/bs";
 import axios from "axios";
+import "./style.scss";
 
 export default function Sidebar() {
   const [users, setUsers] = useState([]);
